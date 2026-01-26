e1a31b9f17d74e550ef9f3238f1ccee6b1cb2ead
Steven Renaux <59167761+StevenRenaux@users.noreply.github.com>
2026-01-26 17:51:25 +0100

    fix: asset Gotenberg runtime for AssetMapper or Webpack usage (#228)

--- tests/Twig/GotenbergRuntimeTest.php
+++ tests/Twig/GotenbergRuntimeTest.php
@@ -5,6 +5,8 @@ namespace Sensiolabs\GotenbergBundle\Tests\Twig;
 use PHPUnit\Framework\TestCase;
 use Sensiolabs\GotenbergBundle\Builder\BuilderAssetInterface;
 use Sensiolabs\GotenbergBundle\Twig\GotenbergRuntime;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\AssetMapper\AssetMapperRepository;
 
 class GotenbergRuntimeTest extends TestCase
 {
@@ -59,5 +61,97 @@ class GotenbergRuntimeTest extends TestCase
             '<style>@font-face {font-family: "my_font";src: url("foo.ttf");}</style>',
             $runtime->getFontStyleTag('foo.ttf', 'my_font'),
         );
+    }
+
+    public function testGetAssetUrlWhenAssetMapperRepository(): void
+    {
+        $builder = $this->createMock(BuilderAssetInterface::class);
+        $builder->expects($this->once())
+            ->method('addAsset')
+            ->with('/image/result.png')
+        ;
+
+        $assetMapperRepository = $this->createMock(AssetMapperRepository::class);
+        $assetMapperRepository->expects($this->once())
+            ->method('find')
+            ->with('image/origin.png')
+            ->willReturn('/image/result.png')
+        ;
+
+        $runtime = new GotenbergRuntime(null, $assetMapperRepository);
+        $runtime->setBuilder($builder);
+
+        $path = $runtime->getAssetUrl('image/origin.png');
+
+        $this->assertSame('result.png', $path);
+    }
+
+    public function testGetAssetUrlWhenPackages(): void
+    {
+        $builder = $this->createMock(BuilderAssetInterface::class);
+        $builder->expects($this->once())
+            ->method('addAsset')
+            ->with('image/result.png')
+        ;
+
+        $packages = $this->createMock(Packages::class);
+        $packages->expects($this->once())
+            ->method('getUrl')
+            ->with('image/origin.png')
+            ->willReturn('/image/result.png')
+        ;
+
+        $runtime = new GotenbergRuntime($packages, null);
+        $runtime->setBuilder($builder);
+
+        $path = $runtime->getAssetUrl('image/origin.png');
+
+        $this->assertSame('result.png', $path);
+    }
+
+    public function testGetAssetUrlWhenAssetMapperRepositoryAndPackages(): void
+    {
+        $builder = $this->createMock(BuilderAssetInterface::class);
+        $builder->expects($this->once())
+            ->method('addAsset')
+            ->with('image/result.png')
+        ;
+
+        $packages = $this->createMock(Packages::class);
+        $packages->expects($this->once())
+            ->method('getUrl')
+            ->with('image/origin.png')
+            ->willReturn('/image/result.png')
+        ;
+
+        $assetMapperRepository = $this->createMock(AssetMapperRepository::class);
+        $assetMapperRepository->expects($this->once())
+            ->method('find')
+            ->with('image/origin.png')
+            ->willReturn(null)
+        ;
+
+        $runtime = new GotenbergRuntime($packages, $assetMapperRepository);
+        $runtime->setBuilder($builder);
+
+        $path = $runtime->getAssetUrl('image/origin.png');
+
+        $this->assertSame('result.png', $path);
+    }
+
+    public function testGetAssetUrlWhenMissingAssetMapperRepositoryAndPackages(): void
+    {
+        $builder = $this->createMock(BuilderAssetInterface::class);
+        $builder->expects($this->once())
+            ->method('addAsset')
+            ->with('image/origin.png')
+        ;
+
+        $runtime = new GotenbergRuntime(null, null);
+        $runtime->setBuilder($builder);
+
+        $path = $runtime->getAssetUrl('image/origin.png');
+
+        $this->assertSame('origin.png', $path);
     }
 }
